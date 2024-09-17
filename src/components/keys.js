ec8ca5e96e6681b6eab2c997e1f68d74a5475222
fatuma65 <namalefatuma28@gmail.com>
2024-09-17 13:24:32 +0300

    create keys and add functionality

---
+++ src/components/keys.js
@@ -0,0 +1,6 @@
+export const thirdRow = ['tab', 'Q','W','E','R','T','Y','U','I','O','P', '{','}', 'Return'];
+export const numbers = ['~',1,2,3,4,5,6,7,8,9,0,'-', '+','backspace']
+export const firstRowKeys = ['esc', 'f1', 'f2', 'f3','f4', 'f5', 'f6','f7','f8','f9','f10', 'f11','f12']
+export const fourth = ['capslock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K','L',':', "'", '|']
+export const fifth = ['up','Z', 'X', 'C','V','B', 'N', 'M', '<', '>', '?','Shift']
+export const sixth = ['ctrl', 'alt', 'space']
